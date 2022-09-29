1d6cc6ae9a56d279ea236a4e7d116820f34008ed
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-29 13:38:04 -0400

    refactor form html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-const apiKey = c4cb0eed6f48cdbf2ed519803c8b0164
+// const apiKey = c4cb0eed6f48cdbf2ed519803c8b0164;
 
-const searchEl = document.getElementById('search-el')
+const searchEl = document.getElementById('search')
+const searchInputEl = document.getElementById('search-input')
+
+const handleSearchSubmit = (event) => {
+    // Prevent page from reloading
+    event.preventDefault();
+
+    // Execute API call
+    console.log(`User searched for ${searchInputEl.value}`)
+
+    // Clear input field
+    searchInputEl.value = ''
+}
+
+
+searchEl.addEventListener('submit', handleSearchSubmit)
